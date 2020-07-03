d6516cb686d02d619f5b7357a507e3c5d221af76
Gaute <gautenybo@gmail.com>
2020-07-03 20:27:29 +0200

    Add files via upload

--- _html/script.js
+++ _html/script.js
@@ -2,3 +2,40 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
 ctx.fillText("Hello World", 10, 50);
+
+
+
+
+
+// -----------------------
+// file handeling 
+// -----------------------
+
+function Upload() {
+    var fileUpload = document.getElementById("fileUpload");
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var table = document.createElement("table");
+                var rows = e.target.result.split("\n");
+                for (var i = 0; i < rows.length; i++) {
+                    var cells = rows[i].split(",");
+                    if (cells.length > 1) {
+                        var row = table.insertRow(-1);
+                        for (var j = 0; j < cells.length; j++) {
+                            var cell = row.insertCell(-1);
+                            cell.innerHTML = cells[j];
+                        }
+                    }
+                }
+                var dvCSV = document.getElementById("dvCSV");
+                dvCSV.innerHTML = "";
+                dvCSV.appendChild(table);
+            }
+            reader.readAsText(fileUpload.files[0]);
+
+}
+
+
+
+
+
